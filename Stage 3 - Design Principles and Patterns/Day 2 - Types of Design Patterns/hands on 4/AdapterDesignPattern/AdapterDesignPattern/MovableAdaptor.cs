9493d6aa4f1e71ada165095713d829c8ad4fa4a4
SushantPagam <2088690@cognizant.com>
2022-02-07 11:56:17 +0530

    Stage 3 - Design Priciples and Pattern hands on added

---
+++ Stage 3 - Design Principles and Patterns/Day 2 - Types of Design Patterns/hands on 4/AdapterDesignPattern/AdapterDesignPattern/MovableAdaptor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdapterDesignPattern
+{
+    public interface MovableAdaptor
+    {
+        double getSpeed();
+        double getPrice();
+    }
+}
